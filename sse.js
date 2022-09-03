6e292c86264cbd6bf3315325dde2af80ec28fa5a
Rascal_Two <therealrascaltwo@gmail.com>
2022-09-03 12:30:57 -0400

    Add SSE delay for completeness

--- sse.js
+++ sse.js
@@ -1,4 +1,5 @@
 const Flatted = require('flatted');
+const { MIDDLEWARE_WAIT_TIME } = require('./constants');
 
 const { SSE, REQUESTS } = require('./globals');
 const { delay, normalizeEvent } = require('./helpers')
@@ -9,15 +10,36 @@ const startSSEPiping = async () => {
 		const chunks = SSE.backflow.splice(0, SSE.backflow.length);
 		if (!chunks.length) continue;
 
-		for (const chunk of chunks) chunk.event = await normalizeEvent(chunk.event);
+		const chunksByRequests = {};
+		for (const chunk of chunks) {
+			if (!(chunk.id in chunksByRequests)) chunksByRequests[chunk.id] = [];
+			chunksByRequests[chunk.id].push(chunk)
+			chunksByRequests[chunk.id].sort((a, b) => b.end - a.end)
+		}
+
+
+		const holding = [];
+		const now = Date.now();
+		for (const chunks of Object.values(chunksByRequests)) {
+			const latest = chunks[0];
+			if (now - latest.event.end <= MIDDLEWARE_WAIT_TIME * 2) {
+				delete chunksByRequests[latest.id];
+				holding.push(...chunks);
+			}
+		}
+		SSE.backflow.push(...holding);
+
+		const readyChunks = Object.values(chunksByRequests).flat();
+
+		for (const chunk of readyChunks) chunk.event = await normalizeEvent(chunk.event, REQUESTS.get(chunk.id)?.events.filter(event => event.order < chunk.event.order) || []);
 
 		const newRequests = {};
-		for (const chunk of chunks) {
+		for (const chunk of readyChunks) {
 			const info = REQUESTS.get(chunk.id);
 			if (!info) continue;
 			if (!(chunk.id in newRequests)) newRequests[chunk.id] = { start: info.start, events: [] };
 			newRequests[chunk.id].events.push(chunk.event);
-			newRequests[chunk.id].events.sort((a, b) => a.start - b.start) || a.order - b.order;
+			newRequests[chunk.id].events.sort((a, b) => a.start - b.start || a.order - b.order);
 		}
 
 		const json = Flatted.stringify(newRequests);
